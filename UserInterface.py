07c28aced412fab6305dd171c66d7afdfd3f8b5e
Renju Liu <i.am.renju.liu@gmail.com>
2019-05-07 11:24:32 -0700

    UI template.

--- UserInterface.py
+++ UserInterface.py
@@ -1,3 +1,25 @@
 """
     This file contains the necessary user interface.
 """
+from tkinter import *
+
+def selectIntention(intentions):
+    window = Tk()
+    window.title('Remedial Actions Intention')
+    window.geometry('350x200')
+    lbl = Label(window, text="hello", font=("Times", 30))
+    lbl.grid(column=0, row=0)
+
+    def clicked():
+        lbl.configure(text="Button clicked!")
+
+    btn = Button(window, text="Click me", command=clicked)
+    btn.grid(column=1, row=0)
+    window.mainloop()
+
+def testUI():
+    intentions = ['cool down', 'ventilization']
+    selectIntention(intentions)
+
+if __name__ == '__main__':
+    testUI()
